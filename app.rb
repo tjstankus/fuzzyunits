72649827d117f27effc9b76cfdc9811d824d9c31
TJ Stankus <tjstankus@gmail.com>
2015-01-20 00:08:25 -0500

    Setup database connection for development and test

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require 'bundler'
+Bundler.require
+
 require 'dotenv'
 Dotenv.load
 
@@ -9,6 +12,15 @@ require 'app/routes/activities'
 
 module FocusUnits
   class App < Sinatra::Application
+    configure :development, :test do
+      set :db, Sequel.connect(
+        adapter: 'postgres',
+        host: 'localhost',
+        database: "focusunits_sinatra_#{environment}",
+        user: ENV['DB_USERNAME'],
+        password: ENV['DB_PASSWORD'])
+    end
+
     use Routes::Activities
   end
 end
